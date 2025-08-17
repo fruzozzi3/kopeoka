455059965bd82f2e91dc25b0410be17fa058ed14
fruzozzi3 <124436222+fruzozzi3@users.noreply.github.com>
2025-08-17 18:12:18 +0300

    Update transaction.dart

--- lib/features/savings/models/transaction.dart
+++ lib/features/savings/models/transaction.dart
@@ -1,4 +1,6 @@
-// lib/features/savings/data/models/transaction.dart
+// lib/features/savings/models/transaction.dart
+
+import 'package:my_kopilka/features/savings/models/transaction_enums.dart';
 
 class Transaction {
   final int? id;
@@ -6,6 +8,8 @@ class Transaction {
   final int amount; // Положительное - пополнение, отрицательное - снятие
   final String? notes;
   final DateTime createdAt;
+  final TransactionType type;
+  final TransactionCategory category;
 
   Transaction({
     this.id,
@@ -13,21 +17,27 @@ class Transaction {
     required this.amount,
     this.notes,
     required this.createdAt,
+    required this.type,
+    required this.category,
   });
 
-   Map<String, dynamic> toMap() => {
-        'id': id,
-        'goal_id': goalId,
-        'amount': amount,
-        'notes': notes,
-        'created_at': createdAt.millisecondsSinceEpoch,
-      };
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'goal_id': goalId,
+    'amount': amount,
+    'notes': notes,
+    'created_at': createdAt.millisecondsSinceEpoch,
+    'type': type.index,
+    'category': category.index,
+  };
 
   factory Transaction.fromMap(Map<String, dynamic> map) => Transaction(
-        id: map['id'] as int?,
-        goalId: map['goal_id'] as int,
-        amount: map['amount'] as int,
-        notes: map['notes'] as String?,
-        createdAt: DateTime.fromMillisecondsSinceEpoch(map['created_at'] as int),
-      );
+    id: map['id'] as int?,
+    goalId: map['goal_id'] as int,
+    amount: map['amount'] as int,
+    notes: map['notes'] as String?,
+    createdAt: DateTime.fromMillisecondsSinceEpoch(map['created_at'] as int),
+    type: TransactionType.values[map['type'] as int],
+    category: TransactionCategory.values[map['category'] as int],
+  );
 }
