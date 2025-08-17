67ab40810600607cffff2b5bc91425fb1176ea26
fruzozzi3 <124436222+fruzozzi3@users.noreply.github.com>
2025-08-17 19:01:24 +0300

    Update transaction_enums.dart

--- lib/features/savings/models/transaction_enums.dart
+++ lib/features/savings/models/transaction_enums.dart
@@ -1,3 +1,5 @@
+// lib/features/savings/models/transaction_enums.dart
+
 enum TransactionType {
   income,
   expense,
@@ -6,26 +8,19 @@ enum TransactionType {
 enum TransactionCategory {
   cash,
   card,
-  transfer,
-  savings,
-  investment,
-  gift,
-  other;
-  
+  bankTransfer,
+  other,
+}
+
+extension TransactionCategoryExtension on TransactionCategory {
   String get displayName {
     switch (this) {
       case TransactionCategory.cash:
         return 'Наличные';
       case TransactionCategory.card:
         return 'Карта';
-      case TransactionCategory.transfer:
-        return 'Перевод';
-      case TransactionCategory.savings:
-        return 'Накопления';
-      case TransactionCategory.investment:
-        return 'Инвестиции';
-      case TransactionCategory.gift:
-        return 'Подарок';
+      case TransactionCategory.bankTransfer:
+        return 'Банковский перевод';
       case TransactionCategory.other:
         return 'Другое';
     }
